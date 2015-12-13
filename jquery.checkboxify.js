f843a54b5ec44f0282c8bbd7d7f7f61913b4b4de
annabels <annabels@clara.net>
2015-12-13 01:50:58 +0000

    upload of initial working version

---
+++ jquery.checkboxify.js
@@ -0,0 +1,42 @@
+(function ($)  {
+	$.widget( 'stog.checkboxify', {
+
+		_create: function() {
+			var that = this;
+			// add in checkboxes
+			this.element.find('tr td:first-child').each( function () {
+				var $this= $(this);
+				var id = $this.html();
+				$this.append('<br><input type="checkbox" class="checkboxify" data-id="' + id + '">');
+			});
+			// add buttons
+			if(this.options.buttons) {
+				var nButtons = this.options.buttons.length;
+				buttDiv = $('<div></div>');
+				this.element.after(buttDiv);
+				for(var i=0; i<nButtons; i++) {
+					but = this.options.buttons[i];
+					newButton = $('<button>' + but.label + '</button>')
+					newButton.data('checkboxify-action', but.action)
+					.data('checkboxify-params', but.params)
+					.click(function() {
+						$this = $(this);
+						var all_params = $.extend($this.data('checkboxify-params'), {id: that.getSelected() });
+						$.redirect($this.data('checkboxify-action'), all_params);
+					});
+					buttDiv.append(newButton)
+				}
+			}
+		},
+
+		getSelected: function() {
+			var a = this.element.find('input.checkboxify:checked').map( function () {
+				return $(this).data('id');				
+			}).get();
+			return a;
+		}
+
+	});
+
+}( jQuery ));
+
